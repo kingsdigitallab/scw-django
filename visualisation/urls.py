5757dadf5df6d8c858ae10e65af3380aca426152
njakeman <neil_jakeman@hotmail.com>
2017-06-30 13:53:22 +0000

    Migrations maps

--- visualisation/urls.py
+++ visualisation/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from visualisation.views import mapview, mapCountry
+from visualisation.views import mapview, mapCountry, migrationLayer, \
+    countryUpdate
 
 urlpatterns = [
+    url(r'^map/(\d+)/(\d+)/$', migrationLayer, name='migration_layer'),
     url(r'^map/(\d+)', mapCountry, name='map_country'),
     url(r'^map/$', mapview, name='map_view'),
-    # url(r'^modules/(?P<pk>\d+)/$', module_detail, name='module_detail'),
+    url(r'^country/$', countryUpdate, name='country_update')
 ]
--- visualisation/views.py
+++ visualisation/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-from visualisation.models import Country
+from visualisation.models import Country, Date, MigrationEvent
 # from haystack.query import SearchQuerySet
 # from haystack.inputs import AutoQuery
+from django.http import JsonResponse
+
+
+def countryUpdate(request):
+    name = request.GET.get('name')
+    country = Country.objects.get(border__name=name)
+    years = []
+    buttons = ''
+    for e in MigrationEvent.objects.filter(origin=country):
+        if e.recognisedrate > 0:
+            if e.date.year not in years:
+                years.append(e.date.year)
+    years = sorted(years)
+    for y in years:
+        buttons += '<input type="button" value="' + str(y) + '"  \
+        onclick="updateMigrationLater(' + str(country.id) + ',' + str(y) + ')" \
+        ></input>'
+
+    return HttpResponse('<h1>' + country.name + '</h1>' + buttons)
 
 
 # Department view
@@ -21,3 +40,27 @@ def mapCountry(request, id):
     country = Country.objects.get(pk=id)
     context['country'] = country
     return render(request, 'visualisation/map.html', context)
+
+
+def migrationLayer(request, id, year):
+    migLayers = []
+    lyr = {}
+    lyr['data'] = []
+    origin = Country.objects.get(pk=id)
+    origPoint = [origin.border.the_geom.centroid.x,
+                 origin.border.the_geom.centroid.y]
+    yr = Date.objects.get(year=year)
+    events = MigrationEvent.objects \
+        .filter(date=yr).filter(origin=origin)
+    for e in events:
+        if e.recognisedrate > 0:
+            destPoint = [e.destination.border.the_geom.centroid.x,
+                         e.destination.border.the_geom.centroid.y]
+            yr = Date.objects.get(year=year)
+            lyr['data'].append({"from": origPoint, "to": destPoint,
+                                "labels": ["", e.destination.name +
+                                           " " + str(e.recognisedrate.
+                                                     __float__())],
+                                "color": "#333333"})
+            migLayers.append(lyr)
+    return JsonResponse(lyr, safe=False)
